430cc852aec383fdd783a68b67e62a78180662e1
jordan huang <jordanhuang99@gmail.com>
2021-01-05 01:12:03 +0100

    finished deploying

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
@@ -13,14 +13,20 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-// const CONNECTION_URL =
-  // "mongodb+srv://huang948:69jh!9MOI6pt@cluster0.dljpq.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+app.get("/", (req, res) => {
+  res.send("Hello to memories API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
